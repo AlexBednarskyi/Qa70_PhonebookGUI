1bdf4b4c90102d8b1edd3ef1a5ed1057a325e890
KristinaTomash <kristitomsh@gmail.com>
2025-11-25 14:04:13 +0200

    Add 20.2

--- src/test/java/com/phonebook/tests/AddContactTests.java
+++ src/test/java/com/phonebook/tests/AddContactTests.java
@@ -1,5 +1,6 @@
 package com.phonebook.tests;
 
+import com.phonebook.core.TestBase;
 import com.phonebook.data.ContactData;
 import com.phonebook.data.UserData;
 import com.phonebook.models.Contact;
@@ -9,10 +10,13 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class AddContactTests extends TestBase{
+public class AddContactTests extends TestBase {
 
     @BeforeMethod
     public void precondition(){
+        if(!app.getUser().isLoginLinkPresent()){
+            app.getUser().clickOnSignOutButton();
+        }
         app.getUser().clickOnLoginLink();
         app.getUser().fillLoginRegisterForm(new User()
                 .setEmail(UserData.email)
--- src/test/java/com/phonebook/tests/CreateAccountTests.java
+++ src/test/java/com/phonebook/tests/CreateAccountTests.java
@@ -1,11 +1,21 @@
 package com.phonebook.tests;
+import com.phonebook.core.TestBase;
 import com.phonebook.data.UserData;
 import com.phonebook.models.User;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-public class CreateAccountTests extends TestBase{
 
+import static com.phonebook.core.ApplicationManager.softAssert;
 
+public class CreateAccountTests extends TestBase {
+
+@BeforeMethod
+public void ensurePrecondition(){
+    if(!app.getUser().isLoginLinkPresent()){
+        app.getUser().clickOnSignOutButton();
+    }
+}
     @Test(enabled = false)
     public void newUserRegisterPositiveTest(){
         //int i =(int)((System.currentTimeMillis()/1000)%3600);
@@ -28,7 +38,11 @@ public class CreateAccountTests extends TestBase{
                 .setEmail(UserData.email)
                 .setPassword(UserData.password));
         app.getUser().clickOnRegistrationButton();
-        Assert.assertTrue(app.getUser().isAlertPresent());
+        softAssert.assertTrue(app.getUser().isAlertPresent());
+        softAssert.assertTrue(app.getUser().isErrorMessagePresent());
+        softAssert.assertAll();
+
     }
+
 
 }
